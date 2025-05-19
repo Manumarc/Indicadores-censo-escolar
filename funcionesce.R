89636c4bd68e8395512f06cb7de63bdaf8f2d57d
Manuel Marcos <151560112+Manumarc@users.noreply.github.com>
2025-05-19 10:09:48 -0500

    Update funcionesce.R

--- funcionesce.R
+++ funcionesce.R
@@ -103,3 +103,230 @@ descargar_bases <- function(año_seleccionado) {
   
   message("Todos los archivos seleccionados han sido descargados y procesados.")
 }
+
+#=========================================================================#
+# Función para el cálculo de indicadores de infraestructura de la escuela #
+#=========================================================================#
+
+cal_indicador <- function(nom_indicador,nom_año){
+  
+  # Libro de códigos #
+  #==================#
+  
+  libcod <- read.xlsx("01 Bases/Libro_de_codigos.xlsx", sheet = 3)
+  
+  # Selección de grupo de indicadores a calcular #
+  #==============================================#
+  
+  # Filtro de datos 
+  
+  base <- libcod %>% 
+    filter(indicador %in% nom_indicador) %>% 
+    filter(año %in% nom_año)
+  
+  # Nombre de la base de datos a llamar 
+  
+  nombre_base <-base %>% 
+    select(nom_base) %>% 
+    pull() %>% 
+    unique()
+  
+  # Nombre de la variable 
+  
+  nombre_preg <- base %>% 
+    select(nom_pregunta) %>% 
+    pull() %>% 
+    unique()
+  
+  # Cambio de nombre de variables #
+  #===============================#
+  
+  # Nombre del cuadro 
+  
+  cuadro <- base %>% 
+    select(CUADRO) %>% 
+    pull() %>% 
+    unique()
+  
+  uniq_cuadro <- base %>% 
+    select(nom_cuadro) %>% 
+    pull() %>% 
+    unique()
+  
+  # Nombre de las preguntas 
+  
+  preguntas <- base %>% 
+    select(codigo_preg, nom_pregunta) %>% 
+    mutate_at(vars(codigo_preg,nom_pregunta),
+              ~str_trim(., "right"))
+  
+  nombres_a_renombrar <- preguntas$nom_pregunta
+  names(nombres_a_renombrar) <- preguntas$codigo_preg
+  
+  # Llamado de base de datos con nombres uniformizados #
+  #====================================================#
+  
+  bd_1 <- read.dbf(paste0("01 Bases/",nombre_base), as.is = TRUE) %>%
+    mutate(CUADRO = case_when(CUADRO %in% cuadro ~ uniq_cuadro,
+                              TRUE ~ CUADRO)) %>%
+    filter(NROCED %in% "11") %>% 
+    filter(CUADRO %in% uniq_cuadro) %>% 
+    rename_with(~nombres_a_renombrar[.x], 
+                .cols = names(nombres_a_renombrar)[names(nombres_a_renombrar) %in% names(.)])
+  
+  if (nom_indicador %in% "Saneamiento"){
+    
+    # Construcción de base de datos final #
+    #=====================================#
+    
+    # Rango de total de terrenos que tiene el local escolar 
+    
+    rango <- bd_1 %>% 
+      select(ID_TERR) %>% 
+      pull() %>% 
+      unique()
+    
+    #Base de datos con el indicador
+    
+    bd_final <- bd_1 %>% 
+      select(CODLOCAL,ID_TERR,!!sym(nombre_preg)) %>% 
+      pivot_wider(names_from = ID_TERR,
+                  values_from = !!sym(nombre_preg)) %>% 
+      rowwise() %>%
+      mutate(no_na = sum(!is.na(c_across(first(rango):last(rango)))),
+             tot_1 = sum(c_across(first(rango):last(rango)) == "1", na.rm = TRUE)) %>%
+      ungroup() %>% 
+      filter(!no_na %in% 0) %>% 
+      mutate(avance = redondear((tot_1/no_na)*100,1)) %>% 
+      mutate(saneamiento = case_when(avance %in% 100 ~ "Saneado",
+                                     avance %in% 0 ~ "No saneado",
+                                     TRUE ~ "Saneado parcial")) %>% 
+      select(CODLOCAL,!!paste0("saneamiento",nom_año) := saneamiento)
+    
+    return(bd_final)
+    
+  } else if(nom_indicador %in% c("Agua","Desagüe","Luz")){
+    
+    # Construcción de base de datos final #
+    #=====================================#
+    
+    claves <- base %>%
+      select(codigo_resp) %>%
+      str_split("; ") %>% 
+      as.data.frame() %>% 
+      pull()
+    
+    serbas_1 <- bd_1 %>% 
+      mutate(conexion = case_when(!!sym(nombre_preg) %in% claves[[1]] ~ "Conexión red pública",
+                                  TRUE ~ "Sin conexión red pública")) %>%
+      select(CODLOCAL, conexion)
+    
+    # Rango de variables duplicadas #
+    #-------------------------------#
+    
+    rango <- serbas_1 %>% 
+      group_by(CODLOCAL) %>%
+      mutate(duplicado = row_number()) %>%
+      ungroup() %>% 
+      select(duplicado) %>% 
+      unique() %>% 
+      pull()
+    
+    # Cálculo de base por código de local único #
+    #-------------------------------------------#
+    
+    if (length(rango) == 1) {
+      
+      serbas_2 <- serbas_1 %>% 
+        group_by(CODLOCAL) %>%
+        mutate(duplicado = row_number()) %>%
+        ungroup() %>% 
+        pivot_wider(names_from = duplicado,
+                    values_from = conexion) %>% 
+        mutate(tot_rp = case_when(
+          !!sym(as.character(rango)) == "Conexión red pública" ~ 1,
+          TRUE ~ 0
+        )) %>% 
+        mutate(serbas = case_when(
+          tot_rp == 0 ~ "Sin conexión red pública",
+          TRUE ~ "Conexión red pública"
+        )) %>% 
+        select(CODLOCAL, !!paste0(nombre_preg,nom_año) := serbas)
+      
+    } else {
+      
+      serbas_2 <- serbas_1 %>% 
+        group_by(CODLOCAL) %>%
+        mutate(duplicado = row_number()) %>%
+        ungroup() %>% 
+        pivot_wider(names_from = duplicado,
+                    values_from = conexion) %>% 
+        rowwise() %>%
+        mutate(tot_rp = sum(
+          c_across(all_of(as.character(rango))) == "Conexión red pública",
+          na.rm = TRUE
+        )) %>%
+        ungroup() %>% 
+        mutate(serbas = case_when(
+          tot_rp == 0 ~ "Sin conexión red pública",
+          TRUE ~ "Conexión red pública"
+        )) %>% 
+        select(CODLOCAL, !!paste0(nombre_preg,nom_año) := serbas)
+    }
+    
+    return(serbas_2)
+    
+  } else if (nom_indicador %in% "Infraestructura aulas"){
+    
+    # Construcción de base de datos final #
+    #=====================================#
+    
+    var_be <- c(nombre_preg[[4]],nombre_preg[[8]],nombre_preg[[9]],nombre_preg[[10]],"ventanabe")
+    var_tot <- c(nombre_preg[[3]],nombre_preg[[5]])
+    
+    infraula_1 <- bd_1 %>% 
+      filter(aula_filtro_1 %in% "1") %>% 
+      mutate(ventanabe = case_when(marcovenbe %in% c("01") & vidriobe %in% c("01") ~ "01",
+                                   is.na(marcovenbe) | is.na(vidriobe) ~ NA_character_,
+                                   TRUE ~ "00")) %>% 
+      mutate(across(
+        all_of(var_be),
+        ~ case_when(.x == "01" ~ 1, TRUE ~ 0),
+        .names = "buen_estado_{.col}"
+      )) %>% 
+      select(CODLOCAL,var_be,var_tot) %>% 
+      group_by(CODLOCAL) %>% 
+      summarise(
+        tot1 = n(),
+        across(
+          all_of(var_tot),
+          ~sum(., na.rm = T),
+          .names = "{.col}"
+        ),
+        across(
+          all_of(var_be),
+          ~sum(.x == "01", na.rm = TRUE),
+          .names = "buen_estado_{.col}"
+        ),
+        .groups = "drop"  # opcional, elimina el agrupamiento
+      ) %>% 
+      mutate(
+        !!paste0("aulapiso",nom_año) := redondear(buen_estado_aulapiso/tot1*100,1),
+        !!paste0("aulatecho",nom_año) := redondear(buen_estado_aulatecho/tot1*100,1),
+        !!paste0("aulapared",nom_año) := redondear(buen_estado_aulapared/tot1*100,1),
+        !!paste0("puerta",nom_año) := redondear(buen_estado_puertabe/puertatot*100,1),
+        !!paste0("ventana",nom_año) := redondear(buen_estado_ventanabe/ventanatot*100,1)
+      ) %>% 
+      select(CODLOCAL,!!paste0("aulapiso",nom_año):!!paste0("ventana",nom_año))
+    
+    return(infraula_1)
+    
+    
+  } else {
+    
+    "Sigue intentando"
+    
+  }
+  
+  
+}
